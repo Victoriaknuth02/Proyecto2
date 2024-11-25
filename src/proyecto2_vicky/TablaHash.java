9d2165f5d6524e88a5d5c9f83bf60797fc130427
Alesia <Alesia0705>
2024-11-25 03:26:09 -0400

    Arreglo de errores

--- src/proyecto2_vicky/TablaHash.java
+++ src/proyecto2_vicky/TablaHash.java
@@ -10,9 +10,9 @@ package proyecto2_vicky;
  */
 public class TablaHash {
 
-    public Lista[] integrantes;
-    public int size;
-    public int count;
+    private Lista[] integrantes;
+    private int size;
+    private int count;
 
     public TablaHash(int size) {
         this.size = size;
@@ -30,52 +30,112 @@ public class TablaHash {
                 hash += hash * 31 + mote_integrante.charAt(i);
             }
         }
-        return hash % this.size;
+        return hash % this.getSize();
     }
 
     public void insertar(Nodo integrante, int modo) {
         int hash = -1;
         if (modo == 1) {
-             hash = this.hash(integrante.nombre.toLowerCase());
+            hash = this.hash(integrante.getNombre().toLowerCase() + ", " + integrante.getNumeral().toLowerCase() + " of his name");
 
         } else if (modo == 2) {
-             hash = this.hash(integrante.mote.toLowerCase());
+            hash = this.hash(integrante.getMote().toLowerCase());
 
         } else if (modo == 3) {
-             hash = this.hash(integrante.titulo.toLowerCase());
+            hash = this.hash(integrante.getTitulo().toLowerCase());
 
         }
-        if (this.integrantes[hash].buscar(integrante.nombre.toLowerCase(), 1) != null && this.integrantes[hash].buscar(integrante.mote.toLowerCase(), 2) != null && this.integrantes[hash].buscar(integrante.titulo.toLowerCase(), 3) != null) {
+        if (hash < 0) {
+            hash *= -1;
+        }
+        if (hash < 0) {
+            hash *= -1;
+        }
+
+        if (this.getIntegrantes()[hash].buscar(integrante.getNombre().toLowerCase() + ", " + integrante.getNumeral().toLowerCase() + " of his name", 1) != null && this.getIntegrantes()[hash].buscar(integrante.getMote().toLowerCase(), 2) != null && this.getIntegrantes()[hash].buscar(integrante.getTitulo().toLowerCase(), 3) != null) {
             return;
         } else {
-            this.integrantes[hash].insertar(integrante);
-            this.count++;
+            this.getIntegrantes()[hash].insertar(integrante);
+            this.setCount(this.getCount() + 1);
         }
     }
 
     public Nodo buscar(String clave, int modo) {
         int hash = this.hash(clave.toLowerCase());
+        if (hash < 0) {
+            hash *= -1;
+        }
         if (modo == 1) {
-            if (this.integrantes[hash].pFirst.nombre.toLowerCase().equals(clave.toLowerCase())) {
-                return this.integrantes[hash].pFirst;
+            String claves[] = clave.split(",");
+//            System.out.println("ppp   "+this.integrantes[hash].pFirst.nombre + ", " + this.integrantes[hash].pFirst.numeral + " of his name");
+                                        System.out.println("----------" +clave);
+
+            if (this.getIntegrantes()[hash].getpFirst().getNombre().trim().toLowerCase().equals(claves[0].trim().toLowerCase()) && claves[1].trim().toLowerCase().contains(this.getIntegrantes()[hash].getpFirst().getNumeral().trim().toLowerCase())) {
+
+                return this.getIntegrantes()[hash].getpFirst();
+                
             } else {
-                return this.integrantes[hash].buscar(clave, 1);
+//                            System.out.println("----------" +clave);
+
+                return this.getIntegrantes()[hash].buscar(clave, 1);
             }
         } else if (modo == 2) {
-            if (this.integrantes[hash].pFirst.mote.toLowerCase().equals(clave.toLowerCase())) {
-                return this.integrantes[hash].pFirst;
+            if (this.getIntegrantes()[hash].getpFirst().getMote().toLowerCase().equals(clave.toLowerCase())) {
+                return this.getIntegrantes()[hash].getpFirst();
             } else {
-                return this.integrantes[hash].buscar(clave, 2);
+                return this.getIntegrantes()[hash].buscar(clave, 2);
             }
         } else if (modo == 3) {
-            if (this.integrantes[hash].pFirst.titulo.toLowerCase().equals(clave.toLowerCase())) {
-                return this.integrantes[hash].pFirst;
+            if (this.getIntegrantes()[hash].getpFirst().getTitulo().toLowerCase().equals(clave.toLowerCase())) {
+                return this.getIntegrantes()[hash].getpFirst();
             } else {
-                return this.integrantes[hash].buscar(clave, 3);
+                return this.getIntegrantes()[hash].buscar(clave, 3);
             }
         }
         return null;
 
+    }
+
+    /**
+     * @return the integrantes
+     */
+    public Lista[] getIntegrantes() {
+        return integrantes;
+    }
+
+    /**
+     * @param integrantes the integrantes to set
+     */
+    public void setIntegrantes(Lista[] integrantes) {
+        this.integrantes = integrantes;
+    }
+
+    /**
+     * @return the size
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * @param size the size to set
+     */
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    /**
+     * @return the count
+     */
+    public int getCount() {
+        return count;
+    }
+
+    /**
+     * @param count the count to set
+     */
+    public void setCount(int count) {
+        this.count = count;
     }
 
 }
